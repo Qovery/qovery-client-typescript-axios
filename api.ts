72692117e1c941950cbd285038e636fe54be0e67
qovery <github@qovery.com>
2023-10-03 13:25:13 +0000

    See https://github.com/Qovery/qovery-openapi-spec/commit/7af580c1b51b9d7b5b8c7c9e4ce8f7cd76661945 from refs/heads/main

--- api.ts
+++ api.ts
@@ -12939,7 +12939,7 @@ export interface ServiceStepMetric {
     'duration_sec'?: number;
 }
 /**
- * The name of the deployment step at the service level: - REGISTRY_CREATE_REPOSITORY: The step to create the repository in the registry. - GIT_CLONE: The step to clone the source code repository.  - BUILD_QUEUEING: The queuing time preceding the actual building step. - BUILD: The step to build the source code. - DEPLOYMENT_QUEUEING: The queuing time preceding the actual deployment step. - DEPLOYMENT: The step to deploy the service.  - ROUTER_DEPLOYMENT: The step to deploy the router.  
+ * The name of the deployment step at the service level: - REGISTRY_CREATE_REPOSITORY: The step to create the repository in the registry. - GIT_CLONE: The step to clone the source code repository.  - BUILD_QUEUEING: The queuing time preceding the actual building step. - BUILD: The step to build the source code. - DEPLOYMENT_QUEUEING: The queuing time preceding the actual deployment step. - DEPLOYMENT: The step to deploy the service.  - ROUTER_DEPLOYMENT: The step to deploy the router.  - MIRROR_IMAGE: The step to mirror the image to the private registry. 
  * @export
  * @enum {string}
  */
@@ -12951,7 +12951,8 @@ export enum ServiceStepMetricNameEnum {
     BUILD = 'BUILD',
     DEPLOYMENT_QUEUEING = 'DEPLOYMENT_QUEUEING',
     DEPLOYMENT = 'DEPLOYMENT',
-    ROUTER_DEPLOYMENT = 'ROUTER_DEPLOYMENT'
+    ROUTER_DEPLOYMENT = 'ROUTER_DEPLOYMENT',
+    MIRROR_IMAGE = 'MIRROR_IMAGE'
 }
 
 /**
