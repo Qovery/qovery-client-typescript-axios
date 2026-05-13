d20a687edb89c3fd1d51b9ad0b21a63794ecf401
qovery <github@qovery.com>
2026-05-13 10:53:59 +0000

    See https://github.com/Qovery/qovery-openapi-spec/commit/b58769b98d4201f5f272c40f30299d1a91f252b0 from refs/heads/main

--- api.ts
+++ api.ts
@@ -2265,6 +2265,38 @@ export interface ArgoCdUnlinkedClusterDetails {
     'applications_count': number;
 }
 /**
+ * 
+ * @export
+ * @interface ArgocdAppManifestResponse
+ */
+export interface ArgocdAppManifestResponse {
+    /**
+     * Git revision the app is currently synced to
+     * @type {string}
+     * @memberof ArgocdAppManifestResponse
+     */
+    'manifest_revision'?: string | null;
+    /**
+     * 
+     * @type {ArgocdAppManifestResponseManifestMetadata}
+     * @memberof ArgocdAppManifestResponse
+     */
+    'manifest_metadata': ArgocdAppManifestResponseManifestMetadata;
+}
+/**
+ * Raw enrichment data fetched from the ArgoCD managed-resources API
+ * @export
+ * @interface ArgocdAppManifestResponseManifestMetadata
+ */
+export interface ArgocdAppManifestResponseManifestMetadata {
+    /**
+     * 
+     * @type {Array<ArgocdManagedResource>}
+     * @memberof ArgocdAppManifestResponseManifestMetadata
+     */
+    'managed_resources'?: Array<ArgocdManagedResource>;
+}
+/**
  * An ArgoCD service
  * @export
  * @interface ArgocdAppResponse
@@ -2357,6 +2389,43 @@ export interface ArgocdAppResponse {
 }
 
 
+/**
+ * 
+ * @export
+ * @interface ArgocdManagedResource
+ */
+export interface ArgocdManagedResource {
+    /**
+     * Kubernetes resource kind (e.g. Deployment, Service)
+     * @type {string}
+     * @memberof ArgocdManagedResource
+     */
+    'kind'?: string;
+    /**
+     * 
+     * @type {string}
+     * @memberof ArgocdManagedResource
+     */
+    'name'?: string;
+    /**
+     * 
+     * @type {string}
+     * @memberof ArgocdManagedResource
+     */
+    'namespace'?: string;
+    /**
+     * JSON-encoded desired Kubernetes manifest
+     * @type {string}
+     * @memberof ArgocdManagedResource
+     */
+    'targetState'?: string;
+    /**
+     * JSON-encoded live Kubernetes manifest
+     * @type {string}
+     * @memberof ArgocdManagedResource
+     */
+    'liveState'?: string;
+}
 /**
  * 
  * @export
@@ -29171,6 +29240,47 @@ export const ArgoCDApiAxiosParamCreator = function (configuration?: Configuratio
             };
         },
         /**
+         * 
+         * @summary Get ArgoCD app manifest enrichment
+         * @param {string} argocdAppId ArgoCD App ID
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        getArgoCdAppManifest: async (argocdAppId: string, options: RawAxiosRequestConfig = {}): Promise<RequestArgs> => {
+            // verify required parameter 'argocdAppId' is not null or undefined
+            assertParamExists('getArgoCdAppManifest', 'argocdAppId', argocdAppId)
+            const localVarPath = `/argocdApp/{argocdAppId}/manifest`
+                .replace(`{${"argocdAppId"}}`, encodeURIComponent(String(argocdAppId)));
+            // use dummy base URL string because the URL constructor only accepts absolute URLs.
+            const localVarUrlObj = new URL(localVarPath, DUMMY_BASE_URL);
+            let baseOptions;
+            if (configuration) {
+                baseOptions = configuration.baseOptions;
+            }
+
+            const localVarRequestOptions = { method: 'GET', ...baseOptions, ...options};
+            const localVarHeaderParameter = {} as any;
+            const localVarQueryParameter = {} as any;
+
+            // authentication ApiKeyAuth required
+            await setApiKeyToObject(localVarHeaderParameter, "Authorization", configuration)
+
+            // authentication bearerAuth required
+            // http bearer authentication required
+            await setBearerAuthToObject(localVarHeaderParameter, configuration)
+
+
+    
+            setSearchParams(localVarUrlObj, localVarQueryParameter);
+            let headersFromBaseOptions = baseOptions && baseOptions.headers ? baseOptions.headers : {};
+            localVarRequestOptions.headers = {...localVarHeaderParameter, ...headersFromBaseOptions, ...options.headers};
+
+            return {
+                url: toPathString(localVarUrlObj),
+                options: localVarRequestOptions,
+            };
+        },
+        /**
          * Retrieve the stored ArgoCD configuration for a cluster. The token is always returned as REDACTED. Requires VIEWER role.
          * @summary Get ArgoCD credentials for a cluster
          * @param {string} clusterId Cluster ID
@@ -29397,6 +29507,19 @@ export const ArgoCDApiFp = function(configuration?: Configuration) {
             return (axios, basePath) => createRequestFunction(localVarAxiosArgs, globalAxios, BASE_PATH, configuration)(axios, localVarOperationServerBasePath || basePath);
         },
         /**
+         * 
+         * @summary Get ArgoCD app manifest enrichment
+         * @param {string} argocdAppId ArgoCD App ID
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        async getArgoCdAppManifest(argocdAppId: string, options?: RawAxiosRequestConfig): Promise<(axios?: AxiosInstance, basePath?: string) => AxiosPromise<ArgocdAppManifestResponse>> {
+            const localVarAxiosArgs = await localVarAxiosParamCreator.getArgoCdAppManifest(argocdAppId, options);
+            const localVarOperationServerIndex = configuration?.serverIndex ?? 0;
+            const localVarOperationServerBasePath = operationServerMap['ArgoCDApi.getArgoCdAppManifest']?.[localVarOperationServerIndex]?.url;
+            return (axios, basePath) => createRequestFunction(localVarAxiosArgs, globalAxios, BASE_PATH, configuration)(axios, localVarOperationServerBasePath || basePath);
+        },
+        /**
          * Retrieve the stored ArgoCD configuration for a cluster. The token is always returned as REDACTED. Requires VIEWER role.
          * @summary Get ArgoCD credentials for a cluster
          * @param {string} clusterId Cluster ID
@@ -29492,6 +29615,16 @@ export const ArgoCDApiFactory = function (configuration?: Configuration, basePat
             return localVarFp.getArgoCdApp(argocdAppId, options).then((request) => request(axios, basePath));
         },
         /**
+         * 
+         * @summary Get ArgoCD app manifest enrichment
+         * @param {string} argocdAppId ArgoCD App ID
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        getArgoCdAppManifest(argocdAppId: string, options?: RawAxiosRequestConfig): AxiosPromise<ArgocdAppManifestResponse> {
+            return localVarFp.getArgoCdAppManifest(argocdAppId, options).then((request) => request(axios, basePath));
+        },
+        /**
          * Retrieve the stored ArgoCD configuration for a cluster. The token is always returned as REDACTED. Requires VIEWER role.
          * @summary Get ArgoCD credentials for a cluster
          * @param {string} clusterId Cluster ID
@@ -29578,6 +29711,18 @@ export class ArgoCDApi extends BaseAPI {
      */
     public getArgoCdApp(argocdAppId: string, options?: RawAxiosRequestConfig) {
         return ArgoCDApiFp(this.configuration).getArgoCdApp(argocdAppId, options).then((request) => request(this.axios, this.basePath));
+    }
+
+    /**
+     * 
+     * @summary Get ArgoCD app manifest enrichment
+     * @param {string} argocdAppId ArgoCD App ID
+     * @param {*} [options] Override http request option.
+     * @throws {RequiredError}
+     * @memberof ArgoCDApi
+     */
+    public getArgoCdAppManifest(argocdAppId: string, options?: RawAxiosRequestConfig) {
+        return ArgoCDApiFp(this.configuration).getArgoCdAppManifest(argocdAppId, options).then((request) => request(this.axios, this.basePath));
     }
 
     /**
